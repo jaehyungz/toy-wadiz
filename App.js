a8c7469b49a279d71a4e09e2e576d6b4687318e2
jaehyungz <blue259@hanmail.net>
2021-04-24 16:39:34 +0900

    wadiz 프로젝트 세팅

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>213</Text>
+      <Text>213</Text>
+      <Text>213</Text>
+      <Text>213</Text>
+      <Text>213</Text>
+
+      <Text>213</Text>
+      <Text>213</Text>
+      <Text>213</Text>
+    </View>
+  );
+};
+export default App;
